78dba8985d0f039d9ad56b0211b42386a19eeb2b
Jing Hu <hu000095@algonquinlive.com>
2024-08-25 18:33:32 -0400

    Completed blocks persistence

--- BLC/Blockchain.go
+++ BLC/Blockchain.go
@@ -1,17 +1,98 @@
 package BLC
 
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+const dbName = "blc.db"
+const blockTableName = "blocks"
+
 type Blockchain struct {
-	Blocks []*Block
+	Tip []byte // The hash of the latest block
+	DB  *bolt.DB
 }
 
 // Create a block chain with Genesis block
 func CreateBlockChainWithGenesis() *Blockchain {
-	genesisBlock := CreateGenesisBlock("Genesis Data ......")
-	return &Blockchain{[]*Block{genesisBlock}}
+	// Open the blc.db data file
+	db, err := bolt.Open(dbName, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var blockHash []byte
+	err = db.Update(func(tx *bolt.Tx) error {
+
+		b := tx.Bucket([]byte(blockTableName))
+		if b == nil {
+			//If table is not existed, Create table
+			b, err = tx.CreateBucket([]byte(blockTableName))
+			if err != nil { // db process Failed
+				return fmt.Errorf("create bucket failed: %s", err)
+			}
+		}
+
+		// Write on table
+		if b != nil { // db process Failed
+			genesisBlock := CreateGenesisBlock("Genesis Data ......")
+			err := b.Put(genesisBlock.Hash, genesisBlock.Serialize())
+			if err != nil { // db process Failed
+				log.Panicf("Failed to PUT block into db! %s", err)
+			}
+
+			err = b.Put([]byte("latest"), genesisBlock.Hash)
+			if err != nil { // db process Failed
+				log.Panicf("Failed to PUT latest hash into db! %s", err)
+			}
+			blockHash = genesisBlock.Hash
+		}
+		// For db process debugging
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &Blockchain{blockHash, db}
 }
 
 // Add a block to the chain
-func (blc *Blockchain) AddBlockToChain(data string, height int64, preHash []byte) {
-	newBlock := NewBlock(data, height, preHash)
-	blc.Blocks = append(blc.Blocks, newBlock)
+func (blc *Blockchain) AddBlockToChain(data string) {
+
+	err := blc.DB.Update(func(tx *bolt.Tx) error {
+		// 1. Get table
+		b := tx.Bucket([]byte(blockTableName))
+
+		// 2. Write on table
+		if b != nil { // db process
+
+			// Get the current latest block
+			lastBlockBytes := b.Get(blc.Tip)
+			lastBlock := Deserialize(lastBlockBytes)
+
+			newBlock := NewBlock(data, lastBlock.Height+1, blc.Tip)
+			err := b.Put(newBlock.Hash, newBlock.Serialize())
+			if err != nil { // db process Failed
+				log.Panicf("Failed to PUT block into db! %s", err)
+			}
+
+			err = b.Put([]byte("latest"), newBlock.Hash)
+			if err != nil { // db process Failed
+				log.Panicf("Failed to PUT latest hash into db! %s", err)
+			}
+			// 3. Update latest hash (Tip)
+			blc.Tip = newBlock.Hash
+		} else {
+			log.Panic("Get bucket failed.")
+		}
+		// For db process debugging
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -1,56 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"github.com/boltdb/bolt"
 	"hu169.ca/simpleBlockChain/BLC"
-	"log"
 )
 
 func main() {
-	block := BLC.NewBlock("Test", 1,
-		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 
-	fmt.Printf("%d\n", block.Nonce)
-	fmt.Printf("%x\n", block.Hash)
-
-	// Open the my.db data file in your current directory.
-	// It will be created if it doesn't exist.
-	db, err := bolt.Open("blc.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		// Create table
-		//b, err := tx.CreateBucket([]byte("blocks"))
-		//if err != nil { // db process Failed
-		//	return fmt.Errorf("create bucket failed: %s", err)
-		//}
-		b := tx.Bucket([]byte("blocks"))
-		// Write on table
-		if b != nil { // db process Failed
-			err := b.Put([]byte("latest"), block.Serialize())
-			if err != nil { // db process Failed
-				log.Panic("Failed to PUT data into db!")
-			}
-		}
-		// For db process debugging
-		return nil
-	})
-
-	if err != nil { // db process Failed
-		log.Panic(err)
-	}
-
-	//fmt.Println("Hi, Block Chain Project in Golang")
-	//blChain := BLC.CreateBlockChainWithGenesis()
+	blChain := BLC.CreateBlockChainWithGenesis()
+	defer blChain.DB.Close()
 
 	// Add more blocks to chain
-	//blChain.AddBlockToChain("Send 100 to A", blChain.Blocks[len(blChain.Blocks)-1].Height+1, blChain.Blocks[len(blChain.Blocks)-1].Hash)
-	//blChain.AddBlockToChain("Send 200 to B", blChain.Blocks[len(blChain.Blocks)-1].Height+1, blChain.Blocks[len(blChain.Blocks)-1].Hash)
-	//blChain.AddBlockToChain("Send 300 to C", blChain.Blocks[len(blChain.Blocks)-1].Height+1, blChain.Blocks[len(blChain.Blocks)-1].Hash)
+	blChain.AddBlockToChain("Send 100 to A")
+	blChain.AddBlockToChain("Send 200 to B")
+	blChain.AddBlockToChain("Send 300 to C")
+	blChain.AddBlockToChain("Send 400 to D")
+	blChain.AddBlockToChain("Send 500 to E")
 
 	//fmt.Println(blChain)
 
