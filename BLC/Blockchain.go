7ef9f156cbff5fa3826cfc168b7282a3a3febc9d
Jing Hu <hu000095@algonquinlive.com>
2024-08-25 14:43:49 -0400

    Add func of AddBlockToChain

--- BLC/Blockchain.go
+++ BLC/Blockchain.go
@@ -9,3 +9,9 @@ func CreateBlockChainWithGenesis() *Blockchain {
 	genesisBlock := CreateGenesisBlock("Genesis Data ......")
 	return &Blockchain{[]*Block{genesisBlock}}
 }
+
+// Add a block to the chain
+func (blc *Blockchain) AddBlockToChain(data string, height int64, preHash []byte) {
+	newBlock := NewBlock(data, height, preHash)
+	blc.Blocks = append(blc.Blocks, newBlock)
+}
--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 	//fmt.Println("Hi, Block Chain Project in Golang")
-	genesisBlc := BLC.CreateBlockChainWithGenesis()
-	fmt.Println(genesisBlc)
-	fmt.Println(genesisBlc.Blocks[0])
+	blChain := BLC.CreateBlockChainWithGenesis()
+
+	// Add more blocks to chain
+	blChain.AddBlockToChain("Send 100 to A", blChain.Blocks[len(blChain.Blocks)-1].Height+1, blChain.Blocks[len(blChain.Blocks)-1].Hash)
+	blChain.AddBlockToChain("Send 200 to B", blChain.Blocks[len(blChain.Blocks)-1].Height+1, blChain.Blocks[len(blChain.Blocks)-1].Hash)
+	blChain.AddBlockToChain("Send 300 to C", blChain.Blocks[len(blChain.Blocks)-1].Height+1, blChain.Blocks[len(blChain.Blocks)-1].Hash)
+
+	fmt.Println(blChain)
+
 }
